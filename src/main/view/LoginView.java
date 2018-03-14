adc21123b407bb333cb003c37e08c491cb4233ce
AlfredoCrescenzi <a.crescenzi@contrader.it>
2018-03-14 17:04:08 +0100

    modifyLoginView

--- src/main/view/LoginView.java
+++ src/main/view/LoginView.java
@@ -16,6 +16,7 @@ public class LoginView implements View {
 
 
     public void showOptions () {
+        System.out.println("-----Accedi con i tuoi dati----");
         System.out.println("-----LOGIN----");
         System.out.println("Nome utente:");
         nomeUtente = getInput();
