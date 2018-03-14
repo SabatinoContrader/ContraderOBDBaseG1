7a7821a2f8a4eab45d7b9b4cff703730f715e52b
AntonioPraticò <totoprat@libero.it>
2018-03-14 11:29:08 +0100

    pushed VianelloProj

---
+++ VianelloProject/src/model/Utente.java
@@ -0,0 +1,27 @@
+package model;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Utente {
+	
+	  private String nome;
+	  private String cognome;
+	  private String email;
+	  private String password;
+	  private int stato;
+	  private int idAzienda;
+	  private Date dataRegistrazione;
+	  private int ruolo;
+	  private String telefono;
+}
