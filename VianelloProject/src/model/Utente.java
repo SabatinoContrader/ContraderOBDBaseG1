cd986cfe7778ab2141a5bb65522299b9876f0993
AntonioPraticò <totoprat@libero.it>
2018-03-14 15:42:14 +0100

    Modified class utente

--- VianelloProject/src/model/Utente.java
+++ VianelloProject/src/model/Utente.java
@@ -8,22 +8,22 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 
 public class Utente {
-	
-	  private String nome;
-	  private String cognome;
-	  private String email;
-	  private String password;
-	  private int stato;
-	  private int idAzienda;
-	  private Date dataRegistrazione;
-	  private int ruolo;
-	  private String telefono;
-	  private ArrayList <Auto> auto;
+
+	private int id;
+	private String nome;
+	private String cognome;
+	private String email;
+	private String password;
+	private int stato;
+	private int idAzienda;
+	private Date dataRegistrazione;
+	private int ruolo;
+	private String telefono;
+	private ArrayList <Auto> auto;
 }
