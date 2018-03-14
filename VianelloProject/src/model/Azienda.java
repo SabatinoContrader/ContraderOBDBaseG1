b39eb13618a0b8e63799ebbfd52787aafa466dd9
AntonioPraticò <totoprat@libero.it>
2018-03-14 17:34:13 +0100

    Modified citta

--- VianelloProject/src/model/Azienda.java
+++ VianelloProject/src/model/Azienda.java
@@ -24,6 +24,6 @@ public class Azienda {
 	  private String longitudine;
 	  private String tipologia;
 	  private Date dataInserimento;
-	  private String città;
+	  private String citta;
 	  private ArrayList<Auto> listAuto;
 }
