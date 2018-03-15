85680f53a53b643887f6d6a169e856c150c4e1d6
AntonioPraticò <totoprat@libero.it>
2018-03-15 12:52:43 +0100

    modified utiliti

--- VianelloProject/src/utility/Utility.java
+++ VianelloProject/src/utility/Utility.java
@@ -1,5 +1,9 @@
 package utility;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Utility {
@@ -8,6 +12,32 @@ public class Utility {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
+	
+	public static void closeConnection(ResultSet rs, PreparedStatement ps,Connection dataConnection, boolean closeConnection) {
+		if (rs!=null) {
+			try {
+				rs.close();
+			} catch (SQLException e1) {
+				System.out.println("Exception in close ResultSet" +e1.getMessage());
+			}
+		}
+		if (ps!=null) {
+			try {
+				ps.close();
+			} catch (SQLException e1) {
+				System.out.println("Exception in close PreparedStatement"+e1.getMessage());
+			}
+		}
+		if (closeConnection) {
+			if (dataConnection != null) {
+				try {
+					dataConnection.close();
+				} catch (SQLException e) {
+					System.out.println("Exception in close connection"+e.getMessage());
+				}
+			}
+		}
+	}
 	
 	
 }
