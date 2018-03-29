e19b4d11ec8a6e738b653b00f70e9de9df567809
CamsteVizzo <c.vizzo@gmail.com>
2018-03-29 10:54:30 +0200

    Spring
    ds

--- AutomotiveJsp/src/main/java/com/vianelloProjectJsp/utils/Application.java
+++
@@ -1,9 +0,0 @@
-package com.vianelloProjectJsp.utils;
-
-public class Application {
-
-    public static void main(String[] args)
-    {
-    }
-
-}
--- automotive/src/main/java/com/project/model/DatiTelemetria.java
+++
@@ -1,9 +0,0 @@
-package com.project.model;
-
-
-public class DatiTelemetria {
-	
-	private int id;
-	private String dati;
-
-}
